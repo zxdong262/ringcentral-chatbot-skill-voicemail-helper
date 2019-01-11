da0702f6a0ac5dbc9e321a46b82b1b8a4a50e538
ZHAO Xudong <zxdong@gmail.com>
2019-01-11 16:56:17 +0800

    seperate message format module

--- src/ringcentral.js
+++ src/ringcentral.js
@@ -7,67 +7,7 @@ import delay from 'timeout-as-promise'
 import copy from 'json-deep-copy'
 import { Service, Bot } from 'ringcentral-chatbot/dist/models'
 import { processMail } from './voicemail-process'
-
-
-/**
- * format google NLP analysis result to glip message string
- */
-
-function sentimentRender(sentiment) {
-  if (!sentiment) {
-    return ''
-  }
-  return '\n**Sentiment:**\n\n' +
-  `* Sentiment score: ${sentiment.score}\n` +
-  `* Sentiment magnitude: ${sentiment.magnitude}\n`
-}
-
-function syntaxRender(syntax) {
-  if (!syntax) {
-    return ''
-  }
-  return '\n**Syntax:**\n\n' +
-    syntax.tokens.reduce((prev, part) => {
-      return prev + `* ${part.partOfSpeech.tag}: ${part.text.content}\n`
-    }, '')
-}
-
-function classificationRender(classification) {
-  if (!classification) {
-    return ''
-  }
-  return '\n**Classification:**\n\n' +
-    classification.categories.reduce((prev, category) => {
-      return prev + `* Name: ${category.name}, Confidence: ${category.confidence}\n`
-    }, '')
-}
-
-function entitySentimentsRender(entitySentiments) {
-  if (!entitySentiments) {
-    return ''
-  }
-  return '\n**Entity sentiment:**\n\n' +
-    entitySentiments.reduce((prev, entity) => {
-      return prev + `* Name: ${entity.name}, Type: ${entity.type}, Score: ${entity.sentiment.score}, Magnitude: ${entity.sentiment.magnitude}\n`
-    }, '')
-}
-
-function resultFormatter (userId, result) {
-  let {
-    text,
-    sentiment,
-    syntax,
-    entitySentiments,
-    classification
-  } = result
-  return `![:Person](${userId}), you got a new voicemail!\n\n` +
-  `Voice mail text: **${text}**\n\n` +
-  'And we did some analysis to the text, here is some result:\n' +
-  classificationRender(classification) +
-  entitySentimentsRender(entitySentiments) +
-  syntaxRender(syntax) +
-  sentimentRender(sentiment)
-}
+import resultFormatter from './message-format'
 
 export const subscribeInterval = () => '/restapi/v1.0/subscription/~?threshold=59&interval=15'
 
